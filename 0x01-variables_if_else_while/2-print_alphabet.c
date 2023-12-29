c5fec706a19936e6ff9b5605536e60c46e152cd4
Scorpius-X <eric.bolade@gmail.com>
2023-12-29 10:02:35 -0800

    betty

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	for (int ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
+
