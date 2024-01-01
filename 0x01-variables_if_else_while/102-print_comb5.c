656dfd51bc194bdd3efed1cca841847eb9acdfea
Scorpius-X <eric.bolade@gmail.com>
2023-12-31 21:23:26 -0800

    finally

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,19 @@
 int main(void)
 {
 	int num1, num2;
-	int digit1, digit2;
 
-	for (num1 = 0; num1 < 10; num1++){
-		for (num2 = 0; num2 < 10; num2++){
-			for (digit1 = 0; digit1 < 10; digit1++) {
-				for (digit2 = 0; digit2 < 10; digit2++) {
-					putchar(num1 + '0');
-					putchar(num2 + '0');
-					putchar(' ');
-					putchar(digit1 + '0');
-					putchar(digit2 + '0');
-					if (digit1 < 9 || digit2 < 9) {
-						putchar(',');
-						putchar(' ');
-					}
-				}
+	for (num1 = 0; num1 <= 99; num1++){
+		for (num2 = num1 + 1; num2 < 100; num2++)
+		{
+			putchar((num1/10) + '0');
+			putchar((num1%10) + '0');
+			putchar(' ');
+			putchar((num2/10) + '0');
+			putchar((num2%10) + '0');
+			if (num1 != 98 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
