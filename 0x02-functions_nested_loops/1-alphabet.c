f7b0e28ed8afafcd6ff4b3a0f2f6b2813b372afe
Scorpius-X <eric.bolade@gmail.com>
2024-01-01 20:44:08 -0800

    ok

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints lowercase alphabets
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	char a[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i != 27; i++)
+	{
+		_putchar(a[i]);
+	}
+	_putchar("\n");
+	return (0);
+}
