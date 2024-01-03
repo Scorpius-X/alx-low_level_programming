44d0d62ae05202e04e48075352cbee63e9875410
Scorpius-X <eric.bolade@gmail.com>
2024-01-02 20:50:37 -0800

    ok

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,2 +1,14 @@
 #include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ *
+ * @n: integer
+ *
+ * Return: returns last digit n % 10
+ */
 
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+	return (last_digit);
+}
