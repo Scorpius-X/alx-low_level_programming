ba3313fd19b2e8077f5e6cc7bbdc8deadee201fd
Scorpius-X <eric.bolade@gmail.com>
2024-01-02 21:05:23 -0800

    ok

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,18 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
-	return (last_digit);
+	int last_digit;
+
+	if (n < 0)
+	{
+		last_digit = (n % 10) * -1;
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = (n % 10);
+		_putchar(last_digit + '0');
+		return (last_digit);
+	}
 }
