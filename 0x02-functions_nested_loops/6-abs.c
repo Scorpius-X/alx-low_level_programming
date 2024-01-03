65f679a6b4e03f257dbbf822907c21689920df7c
Scorpius-X <eric.bolade@gmail.com>
2024-01-02 19:29:09 -0800

    ok

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - calculates the absolute value
+ *
+ * Return: 0 success
+ */
+
+int _abs(int)
+{
+	int x;
+	int y;
+	int z;
+
+	if (x < 0)
+	{
+		_putchar((x * (-1) + '0');
+				}
+				else
+				{
+				_putchar(x + '0');
+				}
+				return (0);
+				}
