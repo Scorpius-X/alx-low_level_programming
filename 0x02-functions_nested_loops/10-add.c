61f7547515f113b8b57cd877aa72cef6267b98c0
Scorpius-X <eric.bolade@gmail.com>
2024-01-07 03:35:05 -0800

    ok

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - adds two integers and printts the result
+ *
+ * @x: integer
+ * @y: integer
+ *
+ * Return: n
+ */
+int add(int x, int y)
+{
+	int n;
+
+	n = x + y;
+	_putchar(n);
+
+	return (n);
+}
