681144d8943c0e3f642e4b405016989ef4f0bbbd
Scorpius-X <eric.bolade@gmail.com>
2024-01-08 07:56:45 -0800

    ol

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints a fibonacci
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	long int i, j, k, count;
+
+	i = 1;
+	j = 2;
+
+	while (count <= 50)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%d", k);
+		if (count != 50)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
