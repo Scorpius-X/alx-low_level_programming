4a9b36d603a680895674f680b3b7b80e3228a3ea
Scorpius-X <eric.bolade@gmail.com>
2024-01-08 05:58:02 -0800

    ok

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,6 +29,12 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar((k % 10) + '0');
 			}
+			else if (k > 99)
+			{
+				_putchar((k / 100) + '0');
+				_putchar((k / 10 % 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 			else
 			{
 				_putchar(' ');
