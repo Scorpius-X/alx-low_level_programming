3f4806852369ac43e256fdd85f0d7d68afe0e506
Scorpius-X <eric.bolade@gmail.com>
2024-01-08 05:12:52 -0800

    ok

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,26 +11,26 @@ void print_times_table(int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		putchar('0');
+		_putchar('0');
 		for (j = 1; j <= n; j++)
 		{
 			if  (n > 15 || n < 0)
 			{
 				return;
 			}
-			putchar(',');
-			putchar(' ');
+			_putchar(',');
+			_putchar(' ');
 			k = i * j;
 
 			if ((k / 10) == 0)
 			{
-				putchar(' ');
-				putchar((k / 10) + '0');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
 			}
 			else
 			{
-				putchar((k / 10) + '0');
-				putchar((k % 10) + '0');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		putchar('\n');
