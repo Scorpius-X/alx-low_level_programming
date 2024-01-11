13bbc6f642910b158afe5b4b1b03c22569a68a42
Scorpius-X <eric.bolade@gmail.com>
2024-01-11 08:03:32 -0800

    ol

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a:integer
+ * @b:integer
+ *
+ * Return:0
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
