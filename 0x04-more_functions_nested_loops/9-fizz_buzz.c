17c0235a430948448cfa3cb9356035de0ddb06fd
Scorpius-X <eric.bolade@gmail.com>
2024-01-13 10:26:14 -0800

    ol

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints fizz buzz
+ *
+ * Return:Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ", i);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ", i);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ", i);
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
