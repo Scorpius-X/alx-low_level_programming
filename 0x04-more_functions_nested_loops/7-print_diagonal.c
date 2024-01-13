2da8d295f60b1c4a423e5ee5280f5832df60b872
Scorpius-X <eric.bolade@gmail.com>
2024-01-13 09:02:06 -0800

    ok

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
@@ -18,9 +18,9 @@ void print_diagonal(int n)
 		}
 		_putchar('\\');
 		_putchar('\n');
-		if (n < 1)
+	}
+	if (n < 1)
 		{
 			_putchar('\n');
 		}
 	}
-}
