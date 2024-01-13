cae78a96b609ec8acf4faa0804b278c51cb1a03c
Scorpius-X <eric.bolade@gmail.com>
2024-01-13 08:51:35 -0800

    ok

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,28 +1,26 @@
 #include "main.h"
+
 /**
  * print_diagonal - prints diagonally
  *
  * @n:integer
  */
+
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < n; i++)
 	{
-		if (n == 0 || n < 0)
+		for (j = 0; j < i; j++)
 		{
-			_putchar('\n');
+			_putchar(' ');
 		}
-		else
+		_putchar('\\');
+		_putchar('\n');
+		if (n < 1)
 		{
-			for (int j = 0; j < i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
