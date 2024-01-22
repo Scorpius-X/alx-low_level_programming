47d097e2a49d362adfb60b8b36932d18af34f519
Scorpius-X <eric.bolade@gmail.com>
2024-01-22 04:31:32 -0800

    ol

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
+#include <stdio.h>
+#include <string.h>
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
