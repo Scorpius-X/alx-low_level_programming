44688793c665a3a9ef0a3b41c66d9ea2da4f315f
Scorpius-X <eric.bolade@gmail.com>
2024-02-06 07:41:47 -0800

    fixed print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,20 +10,19 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)\n");
+			printf("%d %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("%d %s\n", h->len, h->str);
+
 			h = h->next;
 			nodes++;
-			continue;
 		}
-
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		nodes++;
 	}
-
 	return (nodes);
 }
