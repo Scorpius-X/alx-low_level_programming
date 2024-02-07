5cb1aaa9c6e799dcfccffc1b4e0a03042735eb27
Scorpius-X <eric.bolade@gmail.com>
2024-02-06 19:36:04 -0800

    ok

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "list.h"
+
+/**
+ * print_listint - prints a singly linked list
+ * @h: head of the list
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+
+}
